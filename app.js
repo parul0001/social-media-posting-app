01c4e93f5925b35d656a29536105aabb58c5ceb1
parul0001 <parulsharma0900@gmail.com>
2024-06-06 21:48:38 +0530

    add create account functionality

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 
 const userModel = require('./models/user');
+const postModel = require('./models/post');
+
 const cookieParser = require('cookie-parser');
+
+const bcrypt = require('bcrypt');
+const jwt =  require('jsonwebtoken')
+
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -15,6 +21,30 @@ app.get('/', (req, res) => {
     res.render('index');
 })
 
+app.get('/login', (req, res) => {
+    res.render('login');
+})
 
+app.post('/register', async (req, res) => {
+    let {name, username, email, password, age} = req.body;
+    let user = await userModel.findOne({email});
+    if(user) return res.status(500).send('User already registered');
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            await userModel.create({
+                name,
+                username,
+                email,
+                password: hash,
+                age
+            })
+            let token = jwt.sign({email, userId: user._id}, 'secret');
+            res.cookie('token', token)
+            res.send('Registered')
+        })
+    })
+
+})
 
 app.listen(3000)
--- models/user.js
+++ models/user.js
@@ -7,7 +7,13 @@ let userSchema = mongoose.Schema({
     username: String,
     email: String,
     password: String,
-    age: Number
+    age: Number,
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'post'
+        }
+    ]
 })
 
 module.exports = mongoose.model('user', userSchema);
