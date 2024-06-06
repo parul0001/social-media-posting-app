7f7d6b7f2de7336036b075bd18730c9cca24ebd6
parul0001 <parulsharma0900@gmail.com>
2024-06-07 03:50:27 +0530

    add protected route middleware

--- app.js
+++ app.js
@@ -15,7 +15,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser())
 
-
+function isLoggedIn(req, res, next) {
+    if(req.cookies.token === '') res.send('You must log in first')
+    else {
+        let data = jwt.verify(req.cookies.token, 'secret')
+        req.user = data;    
+}
+    next();
+}
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -29,9 +36,22 @@ app.post('/login', async (req, res) => {
     let {email, password} = req.body;
     let user = await userModel.findOne({email});
     if(!user) return res.status(500).send('Something went wrong');
-    console.log(user)
 
-    // bcrypt.compare(password, user)
+
+    bcrypt.compare(password, user.password, (err, result) => {
+        if(result){
+            let token = jwt.sign({email, userId: user._id}, 'secret');
+            res.cookie('token', token);
+            res.status(200).send('You can login');
+        } 
+            
+        else res.redirect('/login')
+    })
+})
+
+app.get('/logout', (req, res) => {
+    res.cookie('token', '');
+    res.redirect('/login')
 })
 
 app.post('/register', async (req, res) => {
@@ -55,6 +75,11 @@ app.post('/register', async (req, res) => {
             res.cookie('token', token)
             res.send('Registered')
 
+})
+
+app.get('/profile', isLoggedIn, (req, res) => {
+    console.log(req.user);
+    res.redirect('login')
 })
 
 app.listen(3000)
